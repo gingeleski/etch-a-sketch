ef1868322ffc84c6a59630118bff19276ee449b1
Randy Gingeleski <rgingeleski@aol.com>
2014-10-01 14:27:24 -0400

    fixed grid spacing, improved dimensions

--- application.js
+++ application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    for (var i = 1; i <= 96; i++) {
+    for (var i = 1; i <= 144; i++) {
     	$('<div />', {
         	'class' : 'blocks',
         	'id'    : 'block' + i
