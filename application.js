1f2d1eaa558d494171187fa37a852f2682d427ff
Randy Gingeleski <rgingeleski@aol.com>
2014-10-05 23:00:14 -0400

    got clear button working

--- application.js
+++ application.js
@@ -1,10 +1,16 @@
+//makes the initial grid
 $(document).ready(function() {
-
     for (var i = 1; i <= 144; i++) {
     	$('<div />', {
         	'class' : 'blocks',
         	'id'    : 'block' + i
     	}).appendTo('#grid');
 	}
+});
 
+//clears the current grid
+$(".clear").click(function(){
+	$(".blocks").css({
+		"background-color":"#D8D8D8", 
+		"opacity":"1"});
 });
