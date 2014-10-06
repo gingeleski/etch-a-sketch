02a733aabcc8aeae0b010e4022b0551574851388
Randy Gingeleski <rgingeleski@aol.com>
2014-10-05 23:23:23 -0400

    re-did the sketch grid entirely, functional now

--- application.js
+++ application.js
@@ -1,16 +1,43 @@
-//makes the initial grid
-$(document).ready(function() {
-    for (var i = 1; i <= 144; i++) {
-    	$('<div />', {
-        	'class' : 'blocks',
-        	'id'    : 'block' + i
-    	}).appendTo('#grid');
-	}
+$(function() {
+  buildGrid();
+  hover();
+  clear();
 });
 
+// makes the table for grid
+function buildGrid() {
+
+  var boxSize = 25;
+
+  $(".grid").append("<table><tbody></table></tbody>");
+
+  for(var i = 0; i < 18; i++) {
+    $("tbody").append("<tr></tr>")
+  };
+
+  for(var i = 0; i < 18; i++) {
+    $("tr").append("<td></td>");
+    $("td").css("height", boxSize);
+    $("td").css("width", boxSize);
+  };
+};
+
+//changes color to the default gray
+function hover() {
+  $("td").hover(function() {
+    $(this).css("background-color", "#a9a9a9");
+  },
+
+  function() {
+    $(this).css("background-color", "#a9a9a9");
+  });
+};
+
 //clears the current grid
-$(".clear").click(function(){
-	$(".blocks").css({
-		"background-color":"#D8D8D8", 
-		"opacity":"1"});
-});
+function clear() {
+	$(".clear").click(function(){
+		$("td").css({
+			"background-color":"#d8d8d8", 
+			"opacity":"1"});
+	});
+};
