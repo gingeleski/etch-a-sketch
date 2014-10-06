6ea5690288e4428e7508b473fc1c04ba43d9dd36
Randy Gingeleski <rgingeleski@aol.com>
2014-10-05 23:31:20 -0400

    implemented random color mode

--- application.js
+++ application.js
@@ -2,6 +2,8 @@ $(function() {
   buildGrid();
   hover();
   clear();
+  randomColor();
+  randomColorHover();
 });
 
 // makes the table for grid
@@ -41,3 +43,22 @@ function clear() {
 			"opacity":"1"});
 	});
 };
+
+//computes a random color in RGB for the random mode
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," +  g + ","+ b + ")"
+};
+
+//random mode - instead of turning blocks gray they turn random colors
+function randomColorHover() {
+  $(".randomness").click(function() {
+    $("td").hover(function() {
+      $(this).css("background-color", randomColor());
+    }, function() {
+      $(this).css("background-color", randomColor());
+    });
+  });
+}
